6c5b366ad1177ad07f95846aff6fb6b8e829e662
dev <dev@bnull.net>
2022-09-19 18:55:47 -0600

    feat: github actions workflow for building x86-64 versions of ubuntu 22-04, ubuntu 20-04, macos 11, macos 12

--- .github/workflows/02-publish.sh
+++ .github/workflows/02-publish.sh
@@ -3,15 +3,13 @@
 OWNER=wmerfalen
 REPO='bls-signatures'
 
-SOURCE_DIR="/home/runner/work/bls-signatures-cli-tool/bls-signatures-cli-tool/"
-
 # Github Personal Access Token will be the first argument to this script
 GITHUB_PAT="$1"
 
 echo "Creating release for Ubuntu x86-64 20.04.05 LTS (Focal) using binary at '${BINARY}'"
 
 curl -4 -L -X POST \
-  -H "Accept: application/vnd.github+json" \ 
+  -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_PAT}" \
   https://api.github.com/repos/"$OWNER"/"$REPO"/releases \
 	-d '{"tag_name":"ubuntu-x86-64-20-04-05-LTS","target_commitish":"main","name":"ubuntu-x86-64-20-04-05-LTS","body":"Ubuntu x86-64 20.04.05 LTS (Focal)","draft":false,"prerelease":false,"generate_release_notes":false}'
